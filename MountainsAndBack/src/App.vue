33e9ab5f0deb64260312d42a3c68fca24bb33c00
Will Saults <wsaults@yahoo.com>
2018-02-22 16:24:46 -0600

    sass-loader and sketch file

--- MountainsAndBack/src/App.vue
+++ MountainsAndBack/src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header-container class="header"></header-container>
+    <main>
+      <router-view/>
+    </main>
+    <!-- <footer-container class="footer"></footer-container> -->
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'header-container': Header
+  }
 }
 </script>
 
-<style>
+<style style scoped lang="scss">
 #app {
+  display: flex;
+  flex-flow: column;
+  min-height: 100vh;
+
+  .header {
+    flex-shrink: 0;
+  }
+
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ MountainsAndBack/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+      <h1>Header</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style style scoped lang="scss">
+    .container {
+        color: white;
+        background-color: #02325f;
+    }
+</style>
